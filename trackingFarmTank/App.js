5e442292b36a37f90dbd3f6ce35e9063cbdd8b35
be niz <fahim.zineb1998@gmail.com>
2022-10-05 18:06:52 +0200

    loging screen

--- trackingFarmTank/App.js
+++ trackingFarmTank/App.js
@@ -1,14 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
+
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import { AuthProvider, useAuth } from "./assets/contexts/AuthProvider";
+import { AuthNavigator } from "./assets/navigators/AuthNavigator";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
+const Root = () => {
+  const { currentUser, loading, error, cleanError } = useAuth();
+
+  if (error) {
+    return (
+      <View style={styles.centeredView}>
+        <Modal animationType="slide" transparent={true} visible={true}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>{error}</Text>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => cleanError()}
+              >
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      </View>
+    );
+  }
+
+  if (loading) {
+    return (
+      <View style={styles.spinnerContainer}>
+        <ActivityIndicator size="large" color="red" />
+      </View>
+    );
+  }
+
+  return currentUser ? <Stack.Navigator>
+        
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator> : <AuthNavigator />;
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +62,52 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  spinnerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+
 });
